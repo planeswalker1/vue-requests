f2b55b2f9a6f0f287ec5c005038cace74265792b
Daniel Munoz <daniel.munozdev@gmail.com>
2018-10-04 18:11:51 -0700

    add start files

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # vue-requests
 
 A website using vue resource to make http requests
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <p>new post form</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  }
+}
+</script>
+
+<style>
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: sans-serif;
+    margin: 0;
+  }
+
+  .container {
+    margin: 0 auto;
+    max-width: 1360px;
+  }
+</style>
