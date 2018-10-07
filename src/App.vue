5dbac23b5c346aed409a07bf46ee978f805e24f3
Daniel Munoz <daniel.munozdev@gmail.com>
2018-10-06 20:13:55 -0700

    Add blogs component, scope css, and add semantic html to root component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="container">
-    <appForm></appForm>
+    <header>
+      <h1 class="heading">
+        Welcome,
+        <br>
+        This website was made to practice Vue and vue-resource
+      </h1>
+
+      <p>Check out these links below:</p>
+      <ul>
+        <li v-for="link in links"><a v-bind:href="link.src">{{ link.title }}</a></li>
+      </ul>
+    </header>
+    <main>
+      <appForm></appForm>
+    </main>
+    <footer>
+      <p>&copy; Vue is cool 2018</p>
+    </footer>
   </div>
 </template>
 
 <script>
 import appForm from './components/new-blog'
+import blogs from './components/blogs'
 
 export default {
   components: {
-    appForm: appForm
+    appForm: appForm,
+    blogs: blogs
   },
   data() {
       return {
-
+      component: 'blogs',
+      links: [
+        {src: 'https://planeswalker1.github.io/', title: 'My Website'},
+        {src: 'https://colorlessenergy.github.io/', title: 'Cool Website'},
+        {src: 'https://github.com/planeswalker1', title: 'My Github'},
+        {src: 'https://github.com/colorlessenergy', title: 'Cool Github'},
+      ],
+      components: [
+        {name: 'blogs', title: 'All Blogs'},
+        {name: 'appForm', title: 'New Blog'},
+      ],
       };
     }
   }
@@ -65,262 +94,10 @@ export default {
     text-decoration: underline;
   }
 
-  h1,
-  form {
-    margin: 0 auto;
-    max-width: 600px;
-  }
-
-  .heading {
-    margin: 0;
-    max-width: unset;
-  }
-
-  h1 {
-    margin-top: 1em;
-  }
-
-  p {
-    margin-bottom: 0;
-  }
-
-  .form__header,
-  .form__fields,
-  .form__controls {
-    padding: 5px;
-  }
-
-  /* form control styles */
-  .group {
-    margin-bottom: 10px;
-  }
-
-  .group__label {
-    display: block;
-    margin: .5em 0;
-  }
-
-  .group__input {
-    background-color: #fff;
-    background-clip: padding-box;
-    border: 1px solid #ced4da;
-    border-radius: .25em;
-    color: #495057;
-    display: block;
-    font-size: 1em;
-    line-height: 1.5;
-    padding: .375em .75em;
-    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-  }
-
-  .group__input:focus {
-    background-color: #fff;
-    border-color: #80bdff;
-    box-shadow: 0 0 0 0.2em rgba(0,123,255,.25);
-    color: #495057;
-    outline: 0;
-  }
-
-  .group__textarea {
-    background-clip: padding-box;
-    background-color: #fff;
-    border: 1px solid #ced4da;
-    border-radius: .25em;
-    color: #495057;
-    display: block;
-    line-height: 1.5;
-    overflow: auto;
-    padding: .375em .75em;
-    resize: vertical;
-    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-  }
-
-  .group__textarea:focus {
-    background-color: #fff;
-    border-color: #80bdff;
-    box-shadow: 0 0 0 0.2em rgba(0,123,255,.25);
-    color: #495057;
-    outline: 0;
-  }
-
-  .group__input,
-  .group__textarea {
-    width: 100%;
-  }
-
-  .btn {
-    background-color: #007bff;
-    border: 1px solid transparent;
-    border-color: #007bff;
-    border-radius: .25em;
-    color: #fff;
-    display: inline-block;
-    font-size: 1em;
-    font-weight: 400;
-    line-height: 1.5;
-    padding: .375em .75em;
-    text-align: center;
-    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-    user-select: none;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    vertical-align: middle;
-    white-space: nowrap;
-  }
-
-  .btn:hover {
-    background-color: #0069d9;
-    border-color: #0062cc;
-    color: #fff;
-    cursor: pointer;
-  }
-
-  .btn:focus {
-    outline: 0;
-    box-shadow: 0 0 0 0.2em rgba(0,123,255,.5);
-  }
-
-  .preview {
-    border: 1px dotted #ccc;
-    margin: 30px 0;
-    padding: 10px 20px;
-  }
-
-  /* radio button styles */
-  .checkbox,
-  .selectbox {
-    margin: 10px 0;
-  }
-
-  /* The container */
-  .radio {
-      cursor: pointer;
-      display: inline-block;
-      margin: 10px;
-      padding-left: 25px;
-      position: relative;
-      -webkit-user-select: none;
-      -moz-user-select: none;
-      -ms-user-select: none;
-      user-select: none;
-  }
-
-  /* Hide the browser's default checkbox */
-  .radio .radio__checkbox {
-      display: none;
-      /* cursor: pointer;
-      opacity: 0;
-      position: absolute; */
-  }
-
-  /* Create a custom checkbox */
-  .radio__checkmark {
-      background-color: #eee;
-      height: 17px;
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 17px;
-  }
-
-  /* On mouse-over, add a grey background color */
-  .radio:hover .radio__checkbox ~ .radio__checkmark {
-      background-color: #ccc;
-  }
-
-  /* When the checkbox is checked, add a blue background */
-  .radio .radio__checkbox:checked ~ .radio__checkmark {
-      background-color: #007bff;
-  }
-
-  /* Create the checkmark/indicator (hidden when not checked) */
-  .radio__checkmark:after {
-      content: "";
-      display: none;
-      position: absolute;
-  }
-
-  /* Show the checkmark when checked */
-  .radio__checkbox:checked ~ .radio__checkmark:after {
-      display: block;
-  }
-
-  /* Style the checkmark/indicator */
-  .radio__checkmark:after {
-      border: solid white;
-      border-width: 0 3px 3px 0;
-      height: 10px;
-      top: 3px;
-      left: 6px;
-      -webkit-transform: rotate(45deg);
-      -ms-transform: rotate(45deg);
-      transform: rotate(45deg);
-      width: 5px;
-  }
-
-  /* select input styles */
-  .select {
-    -moz-appearance: none;
-    -webkit-appearance: none;
-    background: #fff url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 4 5'%3E%3Cpath fill='%23333' d='M2 0L0 2h4zm0 5L0 3h4z'/%3E%3C/svg%3E") no-repeat right .75rem center;
-    background-size: 8px 10px;
-    -webkit-background-size: 8px 10px;
-    border: 1px solid rgba(0,0,0,.15);
-    border-radius: .25em;
-    color: #464a4c;
-    cursor: pointer;
-    display: inline-block;
-    height: calc(2.25em + 2px);
-    line-height: 1.25;
-    max-width: 100%;
-    padding: .375rem 1.75rem .375rem .75rem;
-    vertical-align: middle;
-  }
-
-  .select:focus {
-    border-color: #5cb3fd;
-    cursor: pointer;
-    outline: 0;
-  }
-
-  .select:active {
-    border-color: #5cb3fd;
-    cursor: pointer;
-    outline: 0;
-  }
-
-  button {
-    background-color: #007bff;
-    border: 1px solid transparent;
-    border-color: #007bff;
-    border-radius: .25em;
-    color: #fff;
-    display: inline-block;
-    font-size: 1em;
-    font-weight: 400;
-    line-height: 1.5;
-    padding: .375em .75em;
-    text-align: center;
-    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-    user-select: none;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    vertical-align: middle;
-    white-space: nowrap;
-  }
-
-  button:hover {
-    background-color: #0069d9;
-    border-color: #0062cc;
-    color: #fff;
-    cursor: pointer;
-  }
-
-  button:focus {
-    outline: 0;
-    box-shadow: 0 0 0 0.2em rgba(0,123,255,.5);
+  footer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   @media screen and (max-width: 800px) {
---
+++ src/components/blogs.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="blogs">
+    <h2>All Blog Articles</h2>
+    <div v-for="blog in blogs" class="blog">
+      <h2>{{ blog.title }}</h2>
+      <article>
+        {{ blog.body }}
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        blogs: []
+      };
+    },
+    created() {
+      this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function (data) {
+        console.log(data);
+        this.blogs = data.body.slice(0, 10);
+      });
+    }
+  }
+</script>
+
+<style scoped>
+  .blog {
+    background-color: #eee;
+    border-radius: 10px;
+    margin: 20px 0;
+    padding: 20px;
+  }
+</style>
